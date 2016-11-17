67fde94f17ea18015d2a3d11f0a0381835e855ed
JJ-xyz <JJ_xyz@iCloud.com>
2016-11-16 21:20:00 -0400

    Remove anything related to authentication
    Remove anything related to authentication.

--- public/js/apiController.js
+++ public/js/apiController.js
@@ -3,40 +3,16 @@
 	.module("team-up")
 	.controller("ApiController", ApiController);
 
-
-
-
 	function ApiController($http, $state){
 
 		var self = this;
-    //var rootUrl = "https://task2complete-api.herokuapp.com";  // API heroku url
-    var rootUrl = "http://localhost:3001";  // API local url
-
-    self.signup = signup;
-    self.login = login;
-    self.logout = logout;
-    self.myProfile = myProfile;
-    self.myUpdate = myUpdate;
 
     self.getAllProjects = getAllProjects;
     self.projectEdit = projectEdit;
-    // self.getOneTask = getOneTask;
-    // self.deleteOneTask = deleteOneTask;
-    // self.createTask = createTask;
-    // self.newTask = newTask;
-    // self.updateTask = updateTask;
     self.partialTitle = ''
     self.enote = '';
 
     getAllProjects();
-
-    // *** for second pass review ***
-    // myPendingTasks();
-    // myDelegatedTasks();
-
-    // *-------------------------------------------------------------*
-    // * Application Section --- No separation of concenrs yet       *
-    // *-------------------------------------------------------------*
 
     // --- getAllProjects ---
     function getAllProjects(){
@@ -260,132 +236,5 @@
     //   .catch((err) => { console.log(err) });
     //
     // }
-
-
-
-
-    // *-------------------------------------------------------------*
-    // * Authorization Section --- No separation of concenrs yet     *
-    // *-------------------------------------------------------------*
-
-    // --- Login process, set local storage
-    function login(userPass) {
-      self.enote = '';
-      console.log("userFromAngular>>>", userPass);        // test - 2B deleted
-      $http({
-        method: 'POST',
-        url: `${rootUrl}/api/users/login`,
-        data: {username: userPass.username, password: userPass.password},
-        responseType: 'json'
-      })
-      .then(function(response) {
-        if (response.data.token) {
-          self.currentUser = userPass.username;
-          localStorage.setItem('activeUsername', userPass.username);
-          localStorage.setItem('activeToken', response.data.token);
-          localStorage.setItem('activeUserId', response.data.user.id);
-          console.log("RESPONSE", response);                          // test - 2B deleted
-
-          $state.go('indexAll');
-
-        } else {
-          console.log("RESPONSE", response);                          // test - 2B deleted
-          self.currentUser = '';
-          localStorage.setItem('activeUsername', '');
-          localStorage.setItem('activeToken', '');
-          localStorage.setItem('activeUserId', '');
-
-          self.enote = response.data.message;
-        }
-        console.log("The user is>>>",userPass.username);              // test - 2B deleted
-        console.log("The user_id is>>>", response.data.user.id)       // test - 2B deleted
-        console.log("The token is>>>", response.data.token)           // test - 2B deleted
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    };
-
-     // --- logout process, clear local storage
-    function logout() {
-       self.currentUser = '';
-       localStorage.setItem('activeUsername', '');
-       localStorage.setItem('activeToken', '');
-       localStorage.setItem('activeUserId', '');
-
-       $state.go('home')
-    };
-
-    // --- profile display, for profile edit
-    function myProfile(account) {
-      $http({
-        method: 'GET',
-        url: `${rootUrl}/api/users/${localStorage.activeUserId}`,
-        data: {username: localStorage.activeUsername},
-        headers: {Authorization: `Bearer ${localStorage.activeToken}`},
-        responseType: 'json'
-      })
-      .then(function(response) {
-        self.account = {
-          email: response.data.user.email,
-          e_confirmed: response.data.user.e_confirmed,
-          wantPasswordChange: false
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-      $state.go('user')
-    };
-
-     // --- signup process, create account
-    function signup(account) {
-      console.log("accountFromAngular>>>", account);      // test - 2B deleted
-      $http({
-        method: 'POST',
-        url: `${rootUrl}/api/users`,
-        data: {
-          username: account.username,
-          password: account.password,
-          email: account.email,
-          e_confirmed: account.e_confirmed },   // temp until second pass
-        responseType: 'json'
-      })
-      .then(function(response) {
-        console.log("The user is>>>",account.username);     // test - 2B deleted
-        $state.go('home')
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-     }
-
-     // --- update process, update user profile
-    function myUpdate(account) {
-      console.log("accountFromAngular>>>", account);      // test - 2B deleted
-      self.enote = '';
-      if (self.account.wantPasswordChange) {
-        self.enote = 'Sorry, no password change allowed';
-        $state.go('user');
-        // may complete on second pass - now: no password change allowed
-      } else {
-        $http({
-          method: 'PUT',
-          url: `${rootUrl}/api/users/${localStorage.activeUserId}`,
-          data: {email: self.account.email, e_confirmed: self.account.e_confirmed},   // temp until second pass
-          headers: {Authorization: `Bearer ${localStorage.activeToken}`},
-          responseType: 'json'
-          })
-        .then(function(response) {
-          console.log("RESPONSE", response);     // test - 2B deleted
-          $state.go('indexAll')
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      }
-    }
-
-	}
 
 })()
--- server.js
+++ server.js
@@ -16,12 +16,6 @@ pry = require('pryjs');
 var logger = require('morgan');
 app.use(logger('dev'));
 
-// ============= Express + Handlebars ================ Begin
-// var hbs = require('hbs');
-// app.set("view engine", "hbs");
-//require('handlebars-form-helpers').register(hbs.handlebars);
-// ============= Express + Handlebars ================ End
-
 // --- Database : Mongo DB ---
 var mongoose = require('mongoose');
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/team-up';
@@ -31,26 +25,26 @@ var db = mongoose.connection;
 db.on('error', function(err){ console.log(err); });
 db.once('open', function(){ console.log('[ XYZ ] Connected to Mongo DB'); });
 
-// --- Data Models Loading ---
-var UserModel = require('./models/user.js');
-var ProjectModel = require('./models/project.js');
-var MemberModel = require('./models/member.js');
-
-// --- Midleware : Passport ---  *** for second pass review ****
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-app.use(require('express-session')({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(UserModel.createStrategy());
-passport.serializeUser(UserModel.serializeUser());
-passport.deserializeUser(UserModel.deserializeUser());
+// // --- Data Models Loading ---
+// var UserModel = require('./models/user.js');
+// var ProjectModel = require('./models/project.js');
+// var MemberModel = require('./models/member.js');
+//
+// // --- Midleware : Passport ---  *** for second pass review ****
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local').Strategy;
+//
+// app.use(require('express-session')({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: false
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+//
+// passport.use(UserModel.createStrategy());
+// passport.serializeUser(UserModel.serializeUser());
+// passport.deserializeUser(UserModel.deserializeUser());
 
 // --- Main Routes ---
 app.get('/', function(req, res){
