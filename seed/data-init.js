b262ee5e0d24d3de1f996d723c0260743e9a89a7
JJ-xyz <JJ_xyz@iCloud.com>
2016-11-19 13:31:58 -0400

    Add seed for test data
    Add route to add test data to the deployment.

--- seed/data-init.js
+++ seed/data-init.js
@@ -6,44 +6,44 @@ var router = express.Router();
 var Project = require('../models/project.js');
 
 //*-----------------------------------------------------*
-//* ROUTE :: Unique route to create data in Heroku      *
+//* ROUTE :: Unique route to create test data in Heroku *
 //*-----------------------------------------------------*
-router.get('/reset', function(req, res){
+router.get('/testdata', function(req, res){
 //*-----------------------------------------------------*
 //* Reset the Project Database for testing purpose      *
 //*-----------------------------------------------------*
 // =============== Preloading Members ================
   var aList1 = {
     name: "Mario",
-    skillSet: ["HTML", "JavaScript", "CSS"],
+    skillSet: "HTML, JavaScript, CSS",
     emailContact: "mario@nintendo.com",
     slackContact: "@mario",
     location: "Osaka"
   };
   var aList2 = {
     name: "Luigi",
-    skillSet: ["Angular", "JavaScript", "Mongo"],
+    skillSet: "Angular, JavaScript, Mongo DB",
     emailContact: "luigi@nintendo.com",
     slackContact: "@luigi",
     location: "Osaka"
   };
   var aList3 = {
     name: "Yoshi",
-    skillSet: ["Angular", "Ruby", "Rails"],
+    skillSet: "Angular, Ruby, Rails",
     emailContact: "Yoshi@nintendo.com",
     slackContact: "@Yoshi",
     location: "Kyoto"
   };
   var aList4 = {
     name: "Toad",
-    skillSet: ["Phyton", "jQuery"],
+    skillSet: "Phyton, jQuery",
     emailContact: "Toad@nintendo.com",
     slackContact: "@Toad",
     location: "Kyoto"
   };
   var aList5 = {
     name: "Princess",
-    skillSet: ["Angular", "JavaScript"],
+    skillSet: "Angular, JavaScript",
     emailContact: "princess@nintendo.com",
     slackContact: "@princess",
     location: "Kyoto"
@@ -53,46 +53,46 @@ router.get('/reset', function(req, res){
   .then(function(){
     console.log('[ XYZ ] Projects Data-loading in Progress...')
     return Project.create({
-      name: "Drone flights",
-      description: "Web site to publish the drone flight route upon landing",
-      skillsRequired: ["HTML", "JavaScript"],
+      name: "Flights Reporting",
+      description: "A new web site to publish each Drone (Unmanned Aerial Vehicle) flight trip.  The app will track the flight route (GPS point), location, purpose of the trip, pilot, camera man, drone inspection results before take off and after landing. The GPS route will be acquired using Python language.  The web site will be developed using HTML and JavaScript.  Secure access will be implemented. The application will interface with google maps to display the route.  The database selected for this application is MongoDB",
+      skillsRequired: "HTML, JavaScript, jQuery, Mongo DB",
       dateStart: "2016-12-01",
-      dateEnd: "",
+      dateEnd: "2017-01-30",
       memberList: [aList1, aList2, aList3],
-      baseLocation: "Chile",
+      baseLocation: "Southern California",
     });
   })
   .then(function(){
     return Project.create({
-      name: "Auto Tracker",
-      description: "Web site to track the vehicle GPS positioning on demand",
-      skillsRequired: ["HTML", "JavaScript"],
-      dateStart: "2016-12-21",
-      dateEnd: "",
+      name: "Fleet Tracker",
+      description: "Revamp the existing web site to align the look and feel to current trends.  The web site will include vehicle tracking on demand displayed over a map. All 15 vehicles will be tracked simultaneously over a single map. The site will be developed using HTML, JavaScript and Flexbox (Location Requirement). The existing database is MS-SQL 2008",
+      skillsRequired: "HTML, JavaScript, CSS, Ruby on Rails",
+      dateStart: "2017-01-02",
+      dateEnd: "2017-03-30",
       memberList: [aList2, aList3, aList4],
-      baseLocation: "Osaka",
+      baseLocation: "Berlin",
     });
   })
   .then(function(){
     return Project.create({
-      name: "Team Up Site",
-      description: "web repository of resources inmediately available to engage in web dev projects",
-      skillsRequired: ["HTML", "JavaScript", "jQuery"],
-      dateStart: "2017-01-21",
-      dateEnd: "",
+      name: "Team-Up-Now Site",
+      description: "A web repository of resources inmediately availables to engage in web development within 24 hrs notice.  The site need to change the back-end database from Mongo DB to progreSQL. Authentication feature is part of the upgrade.  The technology in use are HTML, JavaScript, jQuery, Mongo DB",
+      skillsRequired: "HTML, JavaScript, jQuery, Mongo DB, Ruby on Rails",
+      dateStart: "2016-11-23",
+      dateEnd: "2016-12-01",
       memberList: [aList1, aList4, aList5],
-      baseLocation: "Osaka",
+      baseLocation: "San Juan",
     });
   })
   .then(function(){
     return Project.create({
-      name: "Health briefcase",
-      description: "Secured web site with personal emergency health information",
-      skillsRequired: ["Angular", "Phyton", "jQuery"],
-      dateStart: "2017-02-21",
-      dateEnd: "",
+      name: "Personal Health Briefcase",
+      description: "A new web site to hold personal health history to be accesable via mobile device with fingerprint identification.  The site will use multi platform interface including web, android, iOS.  The database will reside in the cloud and bi accessible through an Web API",
+      skillsRequired: "Angular, Phyton, jQuery, SQL, Xcode 4",
+      dateStart: "2017-02-01",
+      dateEnd: "2017-06-30",
       memberList: [aList1, aList3, aList5],
-      baseLocation: "Kioto",
+      baseLocation: "FLa, USA",
     });
   })
   .catch(function(err){
